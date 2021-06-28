6fe7578c99c96a14429f5fc253154aa73433c2b9
bandgarmamta <86586825+bandgarmamta@users.noreply.github.com>
2021-06-28 10:42:27 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript with Reactjs</h1>
+      <p>A one week bootcamp by Shaurya Sinha</p>
+    </div>
+  );
+}
+export default Info;
